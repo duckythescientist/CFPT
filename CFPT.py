19145f1eb37ba9db05ff1f40a2f4e54b8306856e
duckythescientist <seantaylormurphy@gmail.com>
2018-09-27 18:52:55 -0400

    Improve internal buffer and Bytes handling
    Plugins now have better ability to interpret the data as desired.
    The internal clipboard bytearray buffer is used more often,
    but it still deconflicts with the real keyboard.

--- CFPT.py
+++ CFPT.py
@@ -6,11 +6,14 @@ import urllib
 import string
 import codecs
 import re
+# from typing import Union
 
+# byteslike = Union[bytes, bytearray]
 
 # Bodge because nullbytes don't play nicely with the clipboard
-clipboard_string_g = ""  # Holds our clipboard data
-use_clipboard_string_g = False  # True when using our buffer instead of the system's clipboard
+clipboard_string_g = ""  # Holds our remembered clipboard string
+clipboard_bytearray_g = None  # Holds our raw data
+# use_clipboard_data_g = False  # True when using our buffer instead of the system's clipboard
 
 
 """Helper Funcs"""
@@ -21,11 +24,13 @@ safe_printable = string.ascii_letters + string.digits
 
 def b2s(b):
     """Converts a byte array to a string."""
-    return "".join(chr(x) for x in b)
+    # return "".join(chr(x) for x in b)
+    return b.decode("latin_1")
 
 def s2b(s):
     """Converts a string to a byte array."""
-    return bytearray(ord(x) for x in s)
+    # return bytearray(ord(x) for x in s)
+    return bytearray(s.encode("latin_1"))
 
 def greedy_hex(text):
     """Return only the hex characters in a string."""
@@ -38,6 +43,8 @@ def greedy_hex(text):
 
 class CFPT_From(sublime_plugin.TextCommand):
     
+    encoding = None
+
     def gettext(self):
         """Return the text in the highlighted region."""
         region = self.view.sel()[0]
@@ -47,8 +54,23 @@ class CFPT_From(sublime_plugin.TextCommand):
     #     """Disable command when no text selected."""
     #     return self.gettext() is not ""
 
+    # def formatter(self, raw: str): -> Union[(str, byteslike), byteslike, str]
     def formatter(self, raw):
-        """Override this for your custom formatters"""
+        """Override this for your custom formatters
+
+        Depending on how your formatter runs, you can return one of
+        many types of output. 
+
+        Return a `str` if you know what your text should look like.
+
+        Return a `bytes` or `bytearray` if you are returning data,
+        and you are fine with the clipboard having a utf-8
+        representation of your data.
+
+        Return a tuple of `(str, bytes)` or `(str, bytearray)` if
+        the raw data is most important but you still care what the
+        string looks like.
+        """
         return None 
 
     def run(self, edit):
@@ -56,28 +78,50 @@ class CFPT_From(sublime_plugin.TextCommand):
 
         Gets the selected text, formats it, and puts it in the clipboard
         """
-        global use_clipboard_string_g
+        global use_clipboard_data_g
         global clipboard_string_g
-        use_clipboard_string_g = False
+        global clipboard_bytearray_g
+        use_clipboard_data_g = False
         raw = self.gettext()
+        self.encoding = self.view.encoding()
+        if self.encoding == "Undefined":
+            self.encoding = "utf_8"
+        print("Encoding:", self.encoding)
         if raw is not "":  # Did we actually select text?
             try:
-                fmt = self.formatter(raw)  # Do the formatting
+                ret = self.formatter(raw)  # Do the formatting
+                if isinstance(ret, (tuple, list)):
+                    use_clipboard_data_g = True
+                    fstr, fdata = ret
+                    fdata = bytearray(fdata)
+                elif isinstance(ret, (bytes, bytearray)):
+                    use_clipboard_data_g = True
+                    fdata = bytearray(ret)
+                    fstr = ret.decode("utf_8", errors="ignore")
+                elif isinstance(ret, str):
+                    fstr = ret
+                    fdata = bytearray(ret, "utf_8")
             except:
                 sublime.error_message(traceback.format_exc())
-                fmt = None
-            if fmt is not "" and fmt is not None:  # Formatter returned valid text
-                if "\x00" in fmt:  # Null bytes don't work in clipboards
-                    use_clipboard_string_g = True  # Use our custom buffer
-                    clipboard_string_g = fmt 
+                fmt = fdata
+            if fstr is not None and fdata is not None:  # Formatter returned valid text
+                if b"\x00" in fdata:  # Null bytes don't work in clipboards
+                    use_clipboard_data_g = True  # Use our custom buffer
                     print("Setting internal clipboard string")
-                    sublime.error_message("Warning: \nNull byte in text. Using internal scratchpad ONCE.")
-                else:
-                    sublime.set_clipboard(fmt)
+                    sublime.error_message("Warning: \nNull byte in text. Don't rely on standard paste")
+                clipboard_bytearray_g = fdata
+                clipboard_string_g = fstr
+                sublime.set_clipboard(fstr)
+                # Now get the clipboard because sometimes it gets
+                # mangled, and we want to know the current state
+                # to compare against later
+                clipboard_string_g = sublime.get_clipboard()
 
 
 class CFPT_To(sublime_plugin.TextCommand):
     """Pastes text as ?"""
+
+    encoding = None
 
     def put(self, edit, s):
         """Puts 's' into the text area"""
@@ -92,8 +136,13 @@ class CFPT_To(sublime_plugin.TextCommand):
     #     """Disable command when nothing in clipboard."""
     #     return sublime.get_clipboard() is not ""
 
+    # def formatter(self, raw: bytearray): -> Union[str, bytes, bytearray]
     def formatter(self, raw):
-        """Override this for your custom formatters"""
+        """Override this for your custom formatters
+
+        If you don't return a `str` type, your bytes like object
+        will be encoded with the view's encoding
+        """
         return None
 
     def run(self, edit):
@@ -101,22 +150,31 @@ class CFPT_To(sublime_plugin.TextCommand):
 
         Grabs the clipboard, formats it, and puts it into the text area
         """
-        raw = sublime.get_clipboard()
-
-        global use_clipboard_string_g
+        global use_clipboard_data_g
         global clipboard_string_g
+        global clipboard_bytearray_g
 
-        if use_clipboard_string_g:  # We are using our custom buffer
-            raw = clipboard_string_g
-            use_clipboard_string_g = False
-            print("Getting internal clipboard string (length %d)"%len(raw))
+        self.encoding = self.view.encoding()
+        if self.encoding == "Undefined":
+            self.encoding = "utf_8"
+        print("Encoding:", self.encoding)
+        clip = sublime.get_clipboard()
+        if clip != clipboard_string_g:
+            print("Clipboard data changed")
+            fdata = bytearray(clip, "utf_8")
+        elif use_clipboard_data_g:
+            fdata = clipboard_bytearray_g
+        else:
+            fdata = bytearray(clip, "utf_8")
 
         try:
-            fmt = self.formatter(raw)
+            fmt = self.formatter(fdata)
+            if isinstance(fmt, (bytes, bytearray)):
+                fmt = fmt.decode(self.encoding)
         except:
             sublime.error_message(traceback.format_exc())
             fmt = None
-        if fmt is not "" and fmt is not None:
+        if fmt is not None:
             self.put(edit, fmt)
 
 
@@ -144,19 +202,21 @@ Copy From
 Make more of these
 """
 
-class CfptFromRawCommand(CFPT_From):
+class CfptFromNormalCommand(CFPT_From):
     """Raw"""
     def formatter(self, raw):
         return raw
+
+class CfptFromCurrentEncodingCommand(CFPT_From):
+    def formatter(self, raw):
+        return bytearray(raw, self.encoding)
 
 class CfptFromHexCommand(CFPT_From):
     """Hex pairs (ignores non-hex characters so comma/space separated is fine)"""
     def formatter(self, raw):
         h = greedy_hex(raw)
         braw = base64.b16decode(h, casefold=True)
-        sraw = b2s(braw)
-        # print(sraw)
-        return sraw
+        return braw
 
 class CfptFromBase64Command(CFPT_From):
     """Base64 Decode"""
@@ -165,15 +225,12 @@ class CfptFromBase64Command(CFPT_From):
         print(raw)
         braw = base64.b64decode(raw)
         print(braw)
-        sraw = b2s(braw)
-        print(sraw)
-        # print(raw)
-        return sraw
+        return braw
 
 class CfptFromUrlencodeCommand(CFPT_From):
     """URL Decode"""
     def formatter(self, raw):
-        return b2s(urllib.parse.unquote_to_bytes(raw))
+        return urllib.parse.unquote_to_bytes(raw)
 
 class CfptFromSlashxCommand(CFPT_From):
     """\\x?? escapes in string"""
@@ -181,16 +238,15 @@ class CfptFromSlashxCommand(CFPT_From):
         def xescape(match):
             v = match.group(1)
             return chr(int(v,16))
-        return re.sub(r"\\x([0-9a-fA-F]{2})", xescape, raw)
+        ret = re.sub(r"\\x([0-9a-fA-F]{2})", xescape, raw)
+        return ret.encode("latin_1")
 
 class CfptFromBinaryCommand(CFPT_From):
     """Binary octets (ignores non-binary characters so comma/space separated is fine)"""
     def formatter(self, raw):
         b = "".join(x for x in raw if x in "01")
-        sraw = "".join(chr(int(b[i:i+8],2)) for i in range(0,len(b),8))
-        #sraw = b2s(braw)
-        # print(sraw)
-        return sraw
+        raw = bytearray(int(b[i:i+8], 2) for i in range(0, len(b), 8))
+        return raw
 
 
 """
@@ -199,7 +255,12 @@ Paste To
 Make more of these
 """
 
-class CfptToRawCommand(CFPT_To):
+class CfptToNormalCommand(CFPT_To):
+    """Raw"""
+    def formatter(self, raw):
+        return raw.decode("utf-8")
+
+class CfptToDirectEncodingCommand(CFPT_To):
     """Raw"""
     def formatter(self, raw):
         return raw
@@ -207,47 +268,47 @@ class CfptToRawCommand(CFPT_To):
 class CfptToZxhexCommand(CFPT_To):
     """0x??????"""
     def formatter(self, raw):
-        enc = "0x" + b2s(base64.b16encode(s2b(raw)))
+        enc = "0x" + base64.b16encode(raw).decode("ascii")
         return enc
 
 class CfptToHexpairsCommand(CFPT_To):
     """Hex pairs"""
     def formatter(self, raw):
-        enc = b2s(base64.b16encode(s2b(raw)))
+        enc = base64.b16encode(raw).decode("ascii")
         return enc
 
 class CfptToSlashxCommand(CFPT_To):
     """\\x??\\x??\\x??"""
     def formatter(self, raw):
-        enc = "".join("\\x%02X"%ord(x) for x in raw)
+        enc = "".join("\\x%02X" % x for x in raw)
         return enc
 
 class CfptToSlashxsafeCommand(CFPT_To):
     """\\x?? escape anything non-alphanumeric"""
     def formatter(self, raw):
-        enc = "".join(x if x in safe_printable else "\\x%02X"%ord(x) for x in raw)
+        enc = "".join(chr(x) if chr(x) in safe_printable else "\\x%02X" % x for x in raw)
         return enc
 
 class CfptToSlashxminimalCommand(CFPT_To):
     """\\x?? escape anything non-printable"""
     def formatter(self, raw):
-        enc = "".join(x if x in string.printable else "\\x%02X"%ord(x) for x in raw)
+        enc = "".join(chr(x) if chr(x) in string.printable else "\\x%02X" % x for x in raw)
         return enc
 
 class CfptToUrlencodeCommand(CFPT_To):
     """URL Encode"""
     def formatter(self, raw):
-        return urllib.parse.quote_from_bytes(s2b(raw))
+        return urllib.parse.quote_from_bytes(raw)
 
 class CfptToBase64Command(CFPT_To):
     """Base64 Encode"""
     def formatter(self, raw):
-        return b2s(base64.b64encode(s2b(raw)))
+        return base64.b64encode(raw)
 
 class CfptToCarrayCommand(CFPT_To):
     """0x??, 0x??, 0x?? C-style array"""
     def formatter(self, raw):
-        return ", ".join("0x%02X"%ord(x) for x in raw)
+        return ", ".join("0x%02X" % x for x in raw)
 
 
 
--- README.md
+++ README.md
@@ -1,28 +1,28 @@
 # Copy-From Paste-To
 A Sublime Text 3 plugin to provide copy-from and paste-to options
 
-
-Internall, how it works is each of the "Copy-From" commands take the text selection and formats the text to raw 8-bit characters which are then placed into the system clipboard. The "Paste-To" commands read from the system clipboard, run a formatter, and write out the formatted string to each cursor/region in Sublime. Because this uses the system clipboard and internally stores everything as raw 8-bit character strings, the "raw" commands for both CF and PT have the same function as standard copy and paste. 
-
-If a CF or PT operation cannot complete (e.g. an odd number of hex characters selected when copying from hex pairs), the clipboard is untouched, and a popup with the traceback is shown. 
-
 ## Copy-From
 
-* Raw
-* Hex pairs
+* Normal (standard text)
+* Current Encoding (actual bytes)
+* Hex Pairs
+* Binary Octets
 * Base64
 * URL Encoded
 * \x escaped
 
 ## Paste-To
 
-* Raw
+* Normal (standard text)
+* Current Encoding (actual bytes)
 * 0xHex
-* Hex pairs
-* \x escaped
-* Safe ASCII with \x
+* Hex Pairs
+* \x Escaped
+* Safe ASCII With \x Escapes (zealous)
+* Safe ASCII With \x Escapes (minimal)
 * URL Encoded
 * Base64
+* C Array
 
 
 ## Installation
@@ -46,3 +46,8 @@ This is the JSONish file that inserts commands into the rightclick context menu.
 
 ### CFPT.py
 This is where the magic happens
+
+## Inner Workings
+Internally, how it works is each of the "Copy-From" commands take the text selection and formats the text to raw 8-bit characters which are then placed into the system clipboard. The "Paste-To" commands read from the system clipboard, run a formatter, and write out the formatted string to each cursor/region in Sublime. Depending on the formatting, an internal bytearray buffer will be used instead of the clipboard. This lets the plugins have more exact control over the data.
+
+If a CF or PT operation cannot complete (e.g. an odd number of hex characters selected when copying from hex pairs), the clipboard is untouched, and a popup with the traceback is shown. 
